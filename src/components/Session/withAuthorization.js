ab25edff4dfd205bdc9c33687eca933e0ed80ed9
brandon-schabel <brandonschabel1995@gmail.com>
2019-04-28 20:47:56 -0700

    convert withAuthorization & withEmailVerification to functional components

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
@@ -6,38 +6,29 @@ import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
-const withAuthorization = condition => Component => {
-  class WithAuthorization extends React.Component {
-    componentDidMount() {
-      this.listener = this.props.firebase.onAuthUserListener(
-        authUser => {
-          if (!condition(authUser)) {
-            this.props.history.push(ROUTES.SIGN_IN);
-          }
-        },
-        () => this.props.history.push(ROUTES.SIGN_IN),
-      );
-    }
+const withAuthorization = condition => Component => props => {
+  useEffect(() => {
+    const listener = props.firebase.onAuthUserListener(
+      authUser => {
+        if (!condition(authUser)) props.history.push(ROUTES.SIGN_IN);
+      },
+      () => props.history.push(ROUTES.SIGN_IN),
+    );
+    return () => listener(); // Unmount Phase
+  }, []);
 
-    componentWillUnmount() {
-      this.listener();
-    }
-
-    render() {
-      return (
-        <AuthUserContext.Consumer>
-          {authUser =>
-            condition(authUser) ? <Component {...this.props} /> : null
-          }
-        </AuthUserContext.Consumer>
-      );
-    }
-  }
-
-  return compose(
-    withRouter,
-    withFirebase,
-  )(WithAuthorization);
+  return (
+    <AuthUserContext.Consumer>
+      {authUser =>
+        condition(authUser) ? <Component {...props} /> : null
+      }
+    </AuthUserContext.Consumer>
+  );
 };
+
+const WithAuthorization = compose(
+  withRouter,
+  withFirebase,
+)(WithAuthorization);
 
 export default withAuthorization;
